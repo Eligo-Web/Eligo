7d427191a118719dbf7d2ed08da7ed065d07765a
Amir Modaressanavi <smodare1@jhu.edu>
2023-02-21 14:26:10 -0500

    some clean styling added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card(props) {
         <div className="card" style={styles.card}>
             <div className="card-body">
                 <p className="card-title">Computer System Fundamentals</p>
-                <p className="card-text">{props.text}</p>
+                <p className="card-text">EN.601.229</p>
             </div>
         </div>
     );
@@ -15,11 +15,9 @@ const styles = {
     card: {
         width: "18rem",
         height: "7rem",
-        margin: "1rem",
         padding: "1rem",
         backgroundColor: "#B2C6ED",
         borderRadius: "0.25rem",
-        boxShadow: "0 0.125rem 0.25rem rgba(0, 0, 0, 0.075)",
     },
 };
 
