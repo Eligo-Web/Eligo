83d9f47cb824e734339d45ccf140c873e6b70462
Amir Modaressanavi <smodare1@jhu.edu>
2023-02-22 13:56:34 -0500

    top menu bar completed

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,5 +1,25 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { IoIosMenu } from "react-icons/io";
+import "../styles/texts.css";
+import "../styles/menu.css";
+
 function MenuBar(props) {
-  return;
+  return (
+    <Container fluid className="menu-bar">
+      <Row className="menu-bar-items">
+        <Col className="menu-bar-icon">
+          <Button variant="transparent">
+            <IoIosMenu size="3rem" />
+          </Button>
+        </Col>
+        <Col>Your Courses</Col>
+        <Col className="menu-bar-top-right">EN.601.229</Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default MenuBar;
--- src/containers/InAppContainer.jsx
+++ src/containers/InAppContainer.jsx
@@ -1,9 +1,11 @@
 import Card from "../components/Card";
 import PrimaryButton from "../components/Buttons/PrimaryButton";
+import MenuBar from "../components/MenuBar";
 
 function InAppContainer() {
   return (
     <div>
+      <MenuBar />
       <Card />
       <PrimaryButton />
     </div>
