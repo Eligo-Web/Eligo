c410295bd1ec4bb86ff2bbc1888dbcbd3bba3076
Amir Modaressanavi <smodare1@jhu.edu>
2023-02-22 12:17:50 -0500

    fixed styles for card view

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,18 @@
 import "../styles/texts.css";
+import "../styles/buttons.css";
+
+import Button from "react-bootstrap/Button";
 
 function Card(props) {
   return (
-    <div className="card" style={styles.card}>
+    <div className="card clickable">
       <div className="card-body">
         <p className="card-title">Computer System Fundamentals</p>
-        <p className="card-text">EN.601.229</p>
+        <p className="card-subtitle">Dave Hovemeyer</p>
+        <p className="card-id">EN.601.229</p>
       </div>
     </div>
   );
 }
-
-const styles = {
-  card: {
-    width: "18rem",
-    height: "7rem",
-    padding: "1rem",
-    backgroundColor: "#B2C6ED",
-    borderRadius: "0.25rem",
-  },
-};
 
 export default Card;
