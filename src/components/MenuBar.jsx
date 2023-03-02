bd2a119ab490e3b1930410134b3de7ca0474a2b5
Fabiofdez <ffernan9@jh.edu>
2023-03-02 10:40:11 -0500

    replaced menu in sessionview

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,15 +3,24 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { IoIosMenu } from "react-icons/io";
 
 import "../styles/text.css";
 import "../styles/menu.css";
 
 function MenuBar(props) {
+  function openMenu() {
+    document.body.style.overflow = "hidden";
+    const menu = document.getElementById("side-menu");
+    menu.querySelector(".overlay-bg").style.pointerEvents = "all";
+    menu.querySelector(".overlay-bg").style.opacity = 100;
+    menu.querySelector(".menu").style.left = 0;
+    console.log("opened menu");
+  }
   return (
     <Container fluid className="menu-bar">
-      <Button variant="transparent" onClick={props.onClick}>
-        {props.icon}
+      <Button variant="transparent" onClick={openMenu}>
+        <IoIosMenu size="3rem" />
       </Button>
       <Row className="menu-bar-items">
         <Col className="large-title">{props.title}</Col>
--- src/pages/SessionView.jsx
+++ src/pages/SessionView.jsx
@@ -4,16 +4,12 @@ import MenuBar from "../components/MenuBar";
 import Menu from "../components/Menu";
 import Overlay from "../components/Overlay";
 import Container from "react-bootstrap/Container";
-import { IoIosArrowBack } from "react-icons/io";
 
 function SessionView(props) {
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <MenuBar
-        title="Course Name"
-        description="Course Description"
-        icon={<IoIosArrowBack size="2.3rem" />}
-      />
+      <Menu />
+      <MenuBar title="Course Name" description="Course Description" />
       <Overlay title="Poll 1" content={Poll()} />
       <Container className="poll-card-container">
         <PollCard title="Poll 1" />
