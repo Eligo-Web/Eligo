3baa335ab3fe5119dde458481db5e7ba487bce7a
Fabiofdez <ffernan9@jh.edu>
2023-03-22 16:29:39 -0400

    transition fixes

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -49,15 +49,19 @@ export default function Overlay(props) {
     const buttonRow = overlay.querySelector(".button-row");
     const deleteBtns = overlay.querySelector(".delete-popup");
     if (markDelete) {
-      // buttonRow.style.maxHeight = 0;
+      deleteBtns.style.transition = "0.1s ease-in";
+      buttonRow.style.transition = "0.1s ease-out";
       deleteBtns.style.maxHeight = "10rem";
-      // buttonRow.style.margin = "-0.5rem"
       deleteBtns.style.margin = 0;
+      buttonRow.style.maxHeight = 0;
+      buttonRow.style.margin = "-0.5rem"
     } else {
-      // buttonRow.style.maxHeight = "10rem";
+      deleteBtns.style.transition = "0.1s ease-out";
+      buttonRow.style.transition = "0.1s ease-in";
       deleteBtns.style.maxHeight = 0;
-      // buttonRow.style.margin = 0;
       deleteBtns.style.margin = "-0.5rem";
+      buttonRow.style.maxHeight = "10rem";
+      buttonRow.style.margin = 0;
     }
   }, [markDelete]);
 
