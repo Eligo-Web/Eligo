73b2e173c96cff6f9a5ea1fa6c7bcc9704baafe8
Amir Modaressanavi <smodare1@jhu.edu>
2023-06-08 19:02:44 -0400

    saml response created

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,7 +3,11 @@ import axios from "axios";
 
 function SignIn() {
     async function assert() {
-        await axios.post(`${server}/instructor/assert`);
+        await axios.post(`${server}/instructor/assert`, {
+            SAMLResponse: new URLSearchParams(window.location.search).get(
+                "SAMLResponse"
+            ),
+        });
     }
     assert();
     return;
